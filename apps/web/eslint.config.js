3ca0b7d2e1a80f1d3c8741d35a8b3f4c939557ea
Mark <mmansur2@uwo.ca>
2024-12-23 17:27:12 -0500

    update web eslint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,3 @@
-import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -10,8 +9,6 @@ export default tseslint.config(
   baseConfig,
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
